27d8729940872a4b234b41436e2c8d7a4c3ff7f4
Iwansal64 <iwansal64@gmail.com>
2024-12-19 11:03:22 +0700

    Changes to the design

---
+++ client/Home/script.js
@@ -0,0 +1,15 @@
+const time_element = document.getElementById("time");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function update_time() {
+    const today_date = new Date();
+    const time = today_date.getHours() + ":" + today_date.getMinutes();
+    const day = days[today_date.getDay()];
+    const date = today_date.getDate() + "/" + today_date.getMonth() + "/" + today_date.getFullYear();
+    time_element.innerText = date + " " + day;
+}
+
+update_time();
+setInterval(() => {
+    update_time();
+}, 60000);
